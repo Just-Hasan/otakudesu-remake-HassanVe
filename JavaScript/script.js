034e6695a90de1a7d9cb79f7360ab18c84d89fac
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2023-07-26 14:42:57 +0700

    Add image slider

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,6 +2,17 @@ const header = document.querySelector(".header");
 const mobileMenu = document.querySelector(".menu-icon");
 const closeMenu = document.querySelector(".close-icon");
 const navLink = document.querySelectorAll(".nav-link");
+
+// Selected for carousel
+const movieImage = document.getElementById("movieImage");
+const previousButton = document.querySelector(".previousBtn");
+const nextButton = document.querySelector(".nextBtn");
+//
+
+previousButton.addEventListener("click", () => {
+  console.log("You click the previous button");
+});
+
 mobileMenu.addEventListener("click", () => {
   header.classList.add("nav-open");
 });
@@ -15,6 +26,7 @@ navLink.forEach((button) => {
   });
 });
 
+//////////////////////
 // SMOOTH SCROLLING ANIMATION
 
 const allLinks = document.querySelectorAll("a:link");
@@ -38,3 +50,60 @@ allLinks.forEach((link) => {
     }
   });
 });
+
+//
+
+//////////////////////
+// Making the carousel
+
+// Define an array of image sources and their corresponding anime info
+
+const images = [
+  {
+    src: "Images/Compressed/suzume_1.webp",
+    name: "Suzume no Tojimori",
+    score: "Score: 8.8",
+  },
+  {
+    src: "Images/Compressed/tenki no ko.webp",
+    name: "Tenki no Ko",
+    score: "Score: 8.5",
+  },
+  {
+    src: "Images/Compressed/kimi no Nawa.webp",
+    name: "Kimi no Nawa",
+    score: "Score: 9.5",
+  },
+];
+
+let currentIndex = 0;
+
+// Function to update the movie image and anime info
+const updateMovieInfo = function () {
+  movieImage.src = images[currentIndex].src;
+  document.querySelector(".recommended-anime-name").textContent =
+    images[currentIndex].name;
+  document.querySelector(".recommended-anime-score").textContent =
+    images[currentIndex].score;
+};
+
+// Function to handle next button click
+const nextImage = function () {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateMovieInfo();
+};
+
+// Function to handle previous button click
+const previousImage = function () {
+  currentIndex = (currentIndex - 1) % images.length;
+  updateMovieInfo();
+};
+
+// Event listeners for button click
+nextButton.addEventListener("click", nextImage);
+previousButton.addEventListener("click", previousImage);
+
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateMovieInfo();
+}, 3000);
