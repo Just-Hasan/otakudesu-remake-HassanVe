c9c5c89f2ac187d4605446cd9853cb94a27a8be5
Just-Hasan <iadore2code@gmail.com>
2023-11-05 16:42:27 +0800

    Update Otakudesu Remake

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,11 +2,18 @@ const header = document.querySelector(".header");
 const mobileMenu = document.querySelector(".menu-icon");
 const closeMenu = document.querySelector(".close-icon");
 const navLink = document.querySelectorAll(".nav-link");
-
+const navigationBar = document.querySelector(".navigation-bar");
+const allLinks = document.querySelectorAll("a:link");
+const slides = document.querySelectorAll(".slide");
+const previousBtn = document.querySelector(".previousBtn");
+const nextBtn = document.querySelector(".nextBtn");
 const movieImage = document.getElementById("movieImage");
 const movieContainer = document.querySelector(".anime-movies-img");
-//
+const finishedAnimeContainer = document.querySelector(
+  ".finished-anime-container"
+);
 
+/////////////////////////////////////[Event]
 mobileMenu.addEventListener("click", () => {
   header.classList.add("nav-open");
 });
@@ -20,39 +27,21 @@ navLink.forEach((button) => {
   });
 });
 
-//////////////////////
-// SMOOTH SCROLLING ANIMATION
-
-const allLinks = document.querySelectorAll("a:link");
-allLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-    console.log(href);
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    //   Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-  });
+/////////////////////////////////////[Smooth scroll]
+navigationBar.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.tagName !== "A") return;
+  const navLinkSection = e.target.getAttribute("href");
+  if (navLinkSection === "#") {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  } else if (navLinkSection !== "#" && navLinkSection.startsWith("#")) {
+    const navLinkSectionDestination = document.querySelector(navLinkSection);
+    navLinkSectionDestination.scrollIntoView({ behavior: "smooth" });
+  }
 });
 
-//
-
+/////////////////////////////////////[Making the slider work]
 let currentIndex = 0;
-
-const slides = document.querySelectorAll(".slide");
-const previousBtn = document.querySelector(".previousBtn");
-const nextBtn = document.querySelector(".nextBtn");
-
 const movingSlider = function (curIndex) {
   slides.forEach((slide, i) => {
     slide.style.transform = `translateX(${100 * (i - curIndex)}%)`;
@@ -82,3 +71,33 @@ previousBtn.addEventListener("click", () => {
 setInterval(function () {
   nextSlide();
 }, 3000);
+
+/////////////////////////////////////[Requesting API]
+const request = fetch("https://api.jikan.moe/v4/top/anime");
+request
+  .then((response) => response.json())
+  .then((animeData) => {
+    const { data } = animeData;
+    const sliced = data.slice(0, 10);
+    console.log(sliced);
+    sliced.forEach((anime) => {
+      const html = `
+      <figure class="anime">
+      <img
+        src="${anime.images.webp.image_url}"
+        alt="${anime.title}"
+      />
+      <p class="anime-details">
+        <ion-icon name="tv-outline" class="anime-icon"></ion-icon>
+        <span class="anime-episodes">${anime.episodes} episodes</span>
+      </p>
+      <p class="anime-date">${anime.status}</p>
+      <div class="anime-info">
+        <p class="anime-names">
+          ${anime.title}
+        </p>
+      </div>
+    </figure>`;
+      finishedAnimeContainer.insertAdjacentHTML("afterbegin", html);
+    });
+  });
